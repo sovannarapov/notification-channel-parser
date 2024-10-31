194c92e6e5e9faa5ebc32a4bb5a94474e5d522e3
sovannarapov <sovannarapov9@gmail.com>
2024-10-31 15:32:24 +0700

    update program

---
+++ NotificationChannelParser.cs
@@ -0,0 +1,16 @@
+﻿namespace NotificationChannelParser
+{
+    public static class NotificationChannelParser
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the notification title: ");
+            var input = Console.ReadLine();
+
+            var channels = ChannelParser.GetNotificationChannels(input);
+            Console.WriteLine(channels.Count > 0
+                ? $"Receive channels: {string.Join(", ", channels)}"
+                : "No valid channels found.");
+        }
+    }
+}
